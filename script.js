715b6ed091d64f0130c596035c522d4c40a480e7
George W. Walker <george@georgewwalker.com>
2016-08-28 12:23:18 -0500

    Update to the time format javascript
    The new time format wasn't sticking, so I added a small delay to it. I
    also changed the padding so it looks a bit nicer.

--- script.js
+++ script.js
@@ -65,13 +65,18 @@ if (localStorage.getItem("searchExpanded") == "false") {
 }
 
 // Make the times in the sidebar look prettier.
-var timesToReplace = document.getElementsByClassName("tg-time-pri");
-for (var i = 0; i < timesToReplace.length; i++) {
-	if (i == 0) {
-		timesToReplace[i].innerHTML = "";
-	} else if (i <= 12) {
-		timesToReplace[i].innerHTML = i + " AM";
-	} else {
-		timesToReplace[i].innerHTML = i - 12 + " PM";
-	}
-}
+window.onload = function() {
+	window.setTimeout(function () {
+		// If we don't wait for the page to load and some additional time, the time format doesn't stick.
+		var timesToReplace = document.getElementsByClassName("tg-time-pri");
+		for (var i = 0; i < timesToReplace.length; i++) {
+			if (i == 0) {
+				timesToReplace[i].innerHTML = "";
+			} else if (i <= 12) {
+				timesToReplace[i].innerHTML = i + " AM";
+			} else {
+				timesToReplace[i].innerHTML = i - 12 + " PM";
+			}
+		}
+	}, 800);
+};
