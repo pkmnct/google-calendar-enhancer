71661bde318be73e958fafd822e4e56e92549f0b
George Walker <george@georgewwalker.com>
2016-08-29 15:04:39 -0500

    Make the buttons match the Google styling.

--- script.js
+++ script.js
@@ -43,13 +43,43 @@ function expandCollapseMenu() {
 }
 
 // Replace the applogo with the buttons.
-document.getElementsByClassName('applogo')[0].innerHTML = "<a id='menuButton' href='#'><img src='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/src/android-menu.svg' height='100%'></a> &nbsp;&nbsp; <a id='searchButton' href='#'><img src='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/src/android-search.svg' height='100%'></a>";
+// document.getElementsByClassName('applogo')[0].innerHTML = "<a id='menuButton' href='#'><img src='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/src/android-menu.svg' height='100%'></a> &nbsp;&nbsp; <a id='searchButton' href='#'><img src='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/src/android-search.svg' height='100%'></a>";
 
-// Add Event Listener for the menu button.
-document.getElementById("menuButton").addEventListener('click', expandCollapseMenu, false);
+// Remove the applogo
+var applogo = document.getElementsByClassName('applogo')[0];
+applogo.parentNode.removeChild(applogo);
 
-// Add Event Listener for the search button.
-document.getElementById("searchButton").addEventListener('click', expandCollapseSearch, false);
+
+// Function to add the menu buttons
+function addMenuButtons() {
+	// Check if the buttons are already added
+	if ((document.getElementById('nav-enhancer-menu')) == null) {
+	
+		// Create the buttons
+		var theMenuText = '<div id="nav-enhancer-menu" tabindex="0" role="button" title="Menu" class="navbuttonouter goog-inline-block"><div class="navbutton nav-enhancer-menu goog-inline-block"></div></div>'
+		var theSearchMenuText = '<div id="nav-search-menu" tabindex="0" role="button" title="Menu" class="navbuttonouter goog-inline-block"><div class="navbutton nav-search-menu goog-inline-block"></div></div>'
+		
+		// Set a variable for the area we are adding the buttons
+		var topLeftNavigation = document.getElementById('topLeftNavigation').getElementsByTagName('tr')[0];
+		
+		// Add the buttons
+		var theSearchMenu = topLeftNavigation.insertCell(0);
+		theSearchMenu.innerHTML = theSearchMenuText;
+		theSearchMenu.classList.add("enhancer-menu-last");
+		var theMenu = topLeftNavigation.insertCell(0);
+		theMenu.innerHTML = theMenuText;
+		
+		// Add Event Listener for the menu button.
+		document.getElementById("nav-enhancer-menu").addEventListener('click', expandCollapseMenu, false);
+
+		// Add Event Listener for the search button.
+		document.getElementById("nav-search-menu").addEventListener('click', expandCollapseSearch, false);
+	}
+	// Make sure the buttons are always there (if we change weeks it removes the buttons).
+	setTimeout(addMenuButtons, 100);
+}
+addMenuButtons();
+
 
 // Collapse what was collapsed before
 if (localStorage.getItem("menuExpanded") == "false") {
